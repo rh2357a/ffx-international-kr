17719d9244a77637718ec6d55c8482d4c9cf89c9
rh2357a <rh2357a@gmail.com>
2025-10-28 17:42:45 +0900

    20251028: 폰트 보정 (1차, #7)
    'ㅏ' , 'ㅑ' 모음으로 인한 부자연스러운 여백 제거

--- scripts/build_init.bat
+++ scripts/build_init.bat
@@ -14,7 +14,7 @@ for %%i in (patch\*.xdelta) do (
 )
 
 echo build font...
-tools\ffxftcx graphics\font\font_kr.bmp !files! file_00455
+tools\ffxftcx graphics\font\font_kr_v2.bmp !files! file_00455
 
 echo assemble 'asm\ffx_international.asm'...
 tools\armips asm\ffx_international.asm
