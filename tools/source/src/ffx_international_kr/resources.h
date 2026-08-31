d493dbec26d833094a34052c7128433b569ff332
rh2357a <rh2357a@gmail.com>
2026-08-31 12:45:30 +0900

    v20260831 릴리즈

--- tools/source/src/ffx_international_kr/resources.h
+++ tools/source/src/ffx_international_kr/resources.h
@@ -4,8 +4,8 @@
 #define APP_AUTHOR      "rh2357a"
 #define APP_NAME        "ffx_internaltional_kr"
 #define APP_FILENAME    "ffx_internaltional_kr.exe"
-#define APP_VERSION     "v20260817"
-#define APP_VERSION_RES 2026, 8, 17, 0
+#define APP_VERSION     "v20260831"
+#define APP_VERSION_RES 2026, 8, 31, 0
 #define APP_DESCRIPTION APP_NAME " " APP_VERSION
 
 #define RES_ID_APP_MANIFEST 1
